f9c7961226e17bcaa47d3df67c6fdfd578ac17a8
stephenzhu01 <82696093+stephenzhu01@users.noreply.github.com>
2022-04-03 14:53:20 +0800

    Create QuestionGride.swift

---
+++ PictureGame/Sources/PictureGame/QuestionGride.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by stephen on 13/3/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct QuestionGride: View {
+    var body: some View {
+        
+        HStack{
+            
+        }
+        
+        Text("hello, world")
+    }
+}
+
+struct QuestionGride_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionGride()
+    }
+}
